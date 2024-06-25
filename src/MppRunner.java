00ef2479c7b984ff661d4a8ea3587cb87d916a4a
OE100 <omer.er10@gmail.com>
2024-06-25 21:23:14 +0300

    final

--- src/MppRunner.java
+++ src/MppRunner.java
@@ -2,12 +2,12 @@ public class MppRunner {
     public static void main(String[] args) {
         for (int i = 1; i <= 64; i *= 2) {
             System.out.println("Threads (" + i + ") operations for BSTFineSet:");
-            TestSet.runTest(new BSTFineSet(), i, 2^17, 90, 9, 10_000);
+            TestSet.runTest(new BSTFineSet(), i, 2^21, 90, 9, 20_000);
         }
         
         for (int i = 1; i <= 64; i *= 2) {
             System.out.println("Threads (" + i + ") operations for BSTOptSet:");
-            TestSet.runTest(new BSTOptSet(), i, 2^17, 90, 9, 10_000);
+            TestSet.runTest(new BSTOptSet(), i, 2^21, 90, 9, 20_000);
         }
     }
 }
